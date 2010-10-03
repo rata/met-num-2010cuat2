3e79e7f0c157761a51f076e6156b26a5980e8593
elmaildematiaz <elmaildematiaz@22ec4344-e33c-cd00-a1cd-bac6cbf4b1c0>
2010-10-03 20:52:42 +0000

    Utiles para el input/output

---
+++ trunk/metnum-tp2/src/io_utils.hpp
@@ -0,0 +1,10 @@
+#ifndef _ARITMETICA_HPP
+#define _ARITMETICA_HPP
+
+#include <fstream>
+
+int leer(char* pags_path, std::ifstream* fpag );
+
+void cerrar(std::ifstream* file);
+
+#endif // ifdef _ARITMETICA_HPP
