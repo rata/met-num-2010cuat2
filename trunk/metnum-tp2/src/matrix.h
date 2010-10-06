65eed30209401a859213e54d9162ed5fdafed0e3
rodrigocc <rodrigocc@22ec4344-e33c-cd00-a1cd-bac6cbf4b1c0>
2010-10-06 04:19:52 +0000

    lu: clarificar comentarios/documentacion de la matriz

--- trunk/metnum-tp2/src/matrix.h
+++ trunk/metnum-tp2/src/matrix.h
@@ -6,13 +6,16 @@
 class matrix
 {
 public:
+	// Crea una matriz de n * m inicializada con ceros
 	matrix(unsigned int n, unsigned int m);
 	~matrix();
+
 //	float operator[][] (int i, int j);
 	void set(unsigned int i, unsigned int j, float num);
 	float get(unsigned int i, unsigned int j);
 	unsigned int cant_rows();
 	unsigned int cant_cols();
+
 	std::string print(void);
 
 private:
