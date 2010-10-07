43fc8a9e7eefdaf3cfe1e52025f4e3645f8124c4
elmaildematiaz <elmaildematiaz@22ec4344-e33c-cd00-a1cd-bac6cbf4b1c0>
2010-10-07 21:44:20 +0000

    Falta normalizar el vector y ver algunos detalles numéricos

--- metnum-tp2/src/jacobis.cpp
+++ metnum-tp2/src/jacobis.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 float p;
 
+void imprimir_vector(int n, float* x)
+{
+	cout << "[";
+	for ( int i= 0 ; i < n-1 ; i++)
+	{
+		cout << x[i] << ", ";
+	}
+	cout << x[n-1] << "]" << endl;
+}
+
 void imprimir(int n, map<int, map<int,float> > &a)
 {
 	for ( int i = 1; i <= n; i++) {
@@ -65,30 +75,70 @@ void parsearW(int n, ifstream &file, map<int, map<int,float> > &a)
 			float c_j = cj((*it_fi).first, a);
 			//Esta linea me suena a que no anda ni en pedo :S
 			(*it_fi).second = (*it_fi).second * c_j;
-			cout << c_j << endl;
+		}
+	}
+}
+
+float* iterarJacobi(int n, map<int, map<int, float> > &a, float* x)
+{
+	float* x_sig = (float *) malloc(sizeof(float) * n);
+	
+	// Inicializo con 1
+	for ( int i = 0 ; i < n ; i++)
+		x_sig[i] = 1;
+
+	// A x_sig le voy restando los elementos de la matriz de la fila
+	// correspondiente
+	map<int, map<int, float> >::iterator it_col;
+	map<int,float>::iterator it_fi;
+
+	for ( it_col = a.begin() ; it_col != a.end(); it_col++ ) {
+		for ( it_fi = (*it_col).second.begin() ; it_fi != (*it_col).second.end(); it_fi++ ) {
+			int i = (*it_fi).first;
+			float elemento = ((*it_fi).second * x[i-1]);
+			x_sig[i-1] = (x_sig[i-1] - elemento);
 		}
 	}
 
+	return x_sig;
 }
-/*
-float* jacobi(int n, map<int, map<int, float> > &a)
+
+float* jacobi(int n, map<int, map<int, float> > &a, int max_iter)
+{
+	// Propongo como solucion el vector nulo
+	float* x = (float *) calloc(sizeof(float), n);
+	int iteracion = 0;
+
+	while(iteracion < max_iter)
+	{
+		iteracion++;
+		float* buff = x;
+		x = iterarJacobi(n, a, x);
+//		cout << "Iteracion " << iteracion <<":\n";
+//		imprimir_vector(n, x);
+		free(buff);
+	}
+	return x;
+}
+
+void normalizar(float* x, int n)
 {
 }
 
-void normailzar(float* x)
-{
-}
-*/
 int main(int argc, char *argv[])
 {
-	if (argc != 4) {
-		cout << "Uso: <prob> <pag-file> <link-file>" << endl;
+	if (argc != 3 && argc != 4 ) {
+		cout << "Uso: <prob> <pag-file> <link-file> [iteraciones_max = 100]" << endl;
 		return 1;
 	}
 	p = atof(argv[1]);
 	char* pags_path = argv[2];
 	char* link_path = argv[3];
-	
+	int max_iter = 100;
+	/*
+	if ( argc == 4 )
+		max_iter = atoi(argv[4]);
+	*/
 	ifstream f_pag, f_link;
 
 	if (leer(pags_path, &f_pag) != 0 || leer(link_path, &f_link))
@@ -105,14 +155,18 @@ int main(int argc, char *argv[])
 
 	parsearW(n, f_link, a);
 
-	imprimir(n, a);
+//	imprimir(n, a);
 
 	cerrar(&f_link);
 	cerrar(&f_pag);
 	
-//	float* x = jacobi(n, a);
+	float* x = jacobi(n, a, max_iter);
 
-//	normalizar(x);
+	normalizar(x, n);
+
+	cout << "Respuesta:\n";
+	imprimir_vector(n, x);
+	free(x);
 
 	return 0;
 }
