f96507f9c2786242f8ed21e793afd5d299012ddb
rodrigocc <rata@users.noreply.github.com>
2010-10-08 02:57:54 +0000

    lu: triangular la matriz
    Tiene prints de debug y deberia triangular la matriz bien salvo error numerico
    (ver comentarios en el codigo)

--- metnum-tp2/src/lu.cpp
+++ metnum-tp2/src/lu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <assert.h>
 #include "matrix.h"
 
 using namespace std;
@@ -14,7 +15,7 @@ ifstream fpags, flinks;
 /* TODO: tomar p por parametro */
 float p = 0.85;
 
-int load_matrix(matrix &m)
+void load_matrix(matrix &m)
 {
 	// Hacemos que m sea igual W
 	int cant_links;
@@ -62,9 +63,55 @@ int load_matrix(matrix &m)
 		}
 	}
 
-	cout << "matriz final " << endl << m.print();
+	cout << "matriz final:" << endl << m.print();
 
-	return 0;
+	return;
+}
+
+// Pone en cero el elemento (row, row_base) usando la fila row_base como base
+// para calcular el coeficiente de gauss
+void triang_row(matrix &m, uint row_base, uint row)
+{
+	assert(m.get(row_base, row_base) != 0);
+
+	float coef = m.get(row, row_base) / m.get(row_base, row_base);
+
+	for (uint j = row_base; j <= m.cant_cols(); j++) {
+		float val = m.get(row, j) - coef * m.get(row_base, j);
+
+		// Imprimimos la cuenta en forma "simbolica"
+		// XXX: Asi vemos claramente que nos deberia dar cero, pero por
+		// error numerico, no da. Que hacemos ? Ni hacemos la cuenta
+		// para este que *debe* dar cero y lo ponemos en cero ? Y si
+		// otro de los numeros tambien deberia dar cero pero no da (y no
+		// es de los que anulamos directamente) ?
+		cout << "haciendo: (" << row << ", " << j << ")" << endl;
+		cout << "m[" << row << ", " << j << "] " <<  " - ";
+			cout << "m[" << row << ", " << row_base <<"]";
+			cout << " / m[" << row_base << ", " << row_base << "] * ";
+			cout << "m[" << row_base << ", " << j << "] ";
+		cout << " = " << val << endl;
+		cout << endl << m.print();
+
+		m.set(row, j, val);
+	}
+}
+
+void triang_col(matrix &m, uint j)
+{
+	for (uint i = j + 1; i <= m.cant_rows(); i++) {
+		triang_row(m, j, i);
+	}
+}
+
+void triang(matrix &m)
+{
+	// Triangulamos todas las columnas menos la ultima (no hay nada debajo
+	// de la diagonal de la ultima columna)
+	for (uint j = 1; j < m.cant_cols(); j++) {
+		cout << "col " << j << endl;
+		triang_col(m, j);
+	}
 }
 
 
@@ -92,6 +139,8 @@ int main(int argc, char *argv[])
 	matrix m(n, n);
 
 	load_matrix(m);
+	triang(m);
+	cout << "matriz triangulada" << endl << m.print();
 
 	fpags.close();
 	flinks.close();
