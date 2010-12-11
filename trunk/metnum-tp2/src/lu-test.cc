0cec6cba660b2cbc77f5db4a6b75b697d55f9b94
rodrigocc <rodrigocc@22ec4344-e33c-cd00-a1cd-bac6cbf4b1c0>
2010-12-11 23:52:08 +0000

    Cambia el test para que pivotee

--- trunk/metnum-tp2/src/lu-test.cc
+++ trunk/metnum-tp2/src/lu-test.cc
@@ -9,9 +9,9 @@ int main()
 {
 	matrix A(3, 4);
 
-	A.set(1,1,-5);	A.set(1,2,2);	A.set(1,3,2);	A.set(1,4,-10);
+	A.set(1,1,3);	A.set(1,2,17);	A.set(1,3,4);	A.set(1,4,-7);
 
-	A.set(2,1,3);	A.set(2,2,17);	A.set(2,3,4);	A.set(2,4,-7);
+	A.set(2,1,-5);	A.set(2,2,2);	A.set(2,3,2);	A.set(2,4,-10);
 
 	A.set(3,1,1);	A.set(3,2,-3);	A.set(3,3,-3);	A.set(3,4,2);
 
